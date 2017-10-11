be5ffafac330813b3475de185fee50c4deb9b632
kdm <kennygprs@gmail.com>
2017-10-11 15:53:16 -0400

    Update

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-"use strict";
+'use strict';
 
-exports.authenticate = require("./lib/openstack").authenticate;
-exports.rescope = require("./lib/openstack").rescope;
+exports.authenticate = require('./lib/openstack').authenticate;
+exports.rescope = require('./lib/openstack').rescope;
 
-exports.nova = require("./lib/nova");
-exports.glance = require("./lib/glance");
-exports.neutron = require("./lib/neutron");
-exports.keystone = require("./lib/keystone");
-exports.gnocchi = require("./lib/gnocchi");
-exports.cinder = require("./lib/cinder");
-exports.heat = require("./lib/heat");
-exports.manila = require("./lib/manila");
-exports.ironic = require("./lib/ironic");
-exports.searchlight = require("./lib/searchlight");
-exports.ceilometer = require("./lib/ceilometer");
-exports.aodh = require("./lib/aodh");
-exports.swift = require("./lib/swift");
+exports.nova = require('./lib/nova');
+exports.glance = require('./lib/glance');
+exports.neutron = require('./lib/neutron');
+exports.keystone = require('./lib/keystone');
+exports.gnocchi = require('./lib/gnocchi');
+exports.cinder = require('./lib/cinder');
+exports.heat = require('./lib/heat');
+exports.manila = require('./lib/manila');
+exports.ironic = require('./lib/ironic');
+exports.searchlight = require('./lib/searchlight');
+exports.ceilometer = require('./lib/ceilometer');
+exports.aodh = require('./lib/aodh');
+exports.swift = require('./lib/swift');
 
-exports.defres = require("./lib/util").defres;
-exports.endpoint = require("./lib/util").endpoint;
+exports.defres = require('./lib/util').defres;
+exports.endpoint = require('./lib/util').endpoint;
